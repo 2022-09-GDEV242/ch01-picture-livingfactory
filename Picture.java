7d9753afbfea442fbed00099f887fcac18698b22
livingfactory <111710162+livingfactory@users.noreply.github.com>
2022-09-01 16:12:19 -0400

    Update Picture.java
    Changed author. Added and altered objects in each method.

--- Picture.java
+++ Picture.java
@@ -1,31 +1,44 @@
 /**
- * This class represents a simple picture. You can draw the picture using
- * the draw method. But wait, there's more: being an electronic picture, it
- * can be changed. You can set it to black-and-white display and back to
- * colors (only after it's been drawn, of course).
- *
- * This class was written as an early example for teaching Java with BlueJ.
+ * The picture displays a man wearing a red chillba hat, and people 
+ * are playing with a ball behind him. It seems that everyone is
+ * enjoying the sunny day outdoors!
  * 
- * @author  Michael Kšlling and David J. Barnes
- * @version 2016.02.29
+ * @author David Yager
+ * @version 2022.09.01
  */
 public class Picture
 {
-    private Square wall;
-    private Square window;
-    private Triangle roof;
+    private Square rightLense;
+    private Square leftLense;
+    private Triangle chillbaHat;
+    private Circle head;
+    private Square neck;
     private Circle sun;
+    private Square background;
+    private Square ground;
+    private Person person1;
+    private Person person2;
+    private Circle ball;
+    
     private boolean drawn;
 
     /**
-     * Constructor for objects of class Picture
+     * Constructors for objects
      */
     public Picture()
     {
-        wall = new Square();
-        window = new Square();
-        roof = new Triangle();  
+        rightLense = new Square();
+        leftLense = new Square();
+        chillbaHat = new Triangle();  
+        head = new Circle();
+        neck = new Square();
         sun = new Circle();
+        background = new Square();
+        ground = new Square();
+        person1 = new Person();
+        person2 = new Person();
+        ball = new Circle();
+        
         drawn = false;
     }
 
@@ -35,27 +48,69 @@ public class Picture
     public void draw()
     {
         if(!drawn) {
-            wall.moveHorizontal(-140);
-            wall.moveVertical(20);
-            wall.changeSize(120);
-            wall.makeVisible();
+            background.changeColor("skyblue");
+            background.changeSize(999);
+            background.moveVertical(-180);
+            background.moveHorizontal(-320);
+            background.makeVisible();
             
-            window.changeColor("black");
-            window.moveHorizontal(-120);
-            window.moveVertical(40);
-            window.changeSize(40);
-            window.makeVisible();
-    
-            roof.changeSize(60, 180);
-            roof.moveHorizontal(20);
-            roof.moveVertical(-60);
-            roof.makeVisible();
-    
+            ground.changeColor("green");
+            ground.changeSize(999);
+            ground.moveVertical(120);
+            ground.moveHorizontal(-320);
+            ground.makeVisible();
+            
+            
             sun.changeColor("yellow");
-            sun.moveHorizontal(100);
-            sun.moveVertical(-40);
-            sun.changeSize(80);
+            sun.changeSize(90);
+            sun.moveVertical(-80);
+            sun.moveHorizontal(160);
             sun.makeVisible();
+            
+            ball.changeColor("black");
+            ball.changeSize(10);
+            ball.moveVertical(120);
+            ball.moveHorizontal(150);
+            ball.makeVisible();
+            
+            person1.moveVertical(40);
+            person1.moveHorizontal(80);
+            person1.makeVisible();
+            
+            person2.moveVertical(25);
+            person2.moveHorizontal(140);
+            person2.makeVisible();
+            
+            head.changeColor("lighttone");
+            head.moveHorizontal(-80);
+            head.moveVertical(20);
+            head.changeSize(180);
+            head.makeVisible();
+    
+            chillbaHat.changeColor("red");
+            chillbaHat.changeSize(120, 240);
+            chillbaHat.moveHorizontal(25);
+            chillbaHat.moveVertical(-120);
+            chillbaHat.makeVisible();
+            
+            neck.changeColor("lighttone");
+            neck.moveHorizontal(-110);
+            neck.moveVertical(160);
+            neck.changeSize(80);
+            neck.makeVisible();
+            
+            rightLense.changeColor("black");
+            rightLense.moveHorizontal(-60);
+            rightLense.moveVertical(40);
+            rightLense.changeSize(40);
+            rightLense.makeVisible();
+            
+            leftLense.changeColor("black");
+            leftLense.moveHorizontal(-120);
+            leftLense.moveVertical(40);
+            leftLense.changeSize(40);
+            leftLense.makeVisible();
+            
             drawn = true;
         }
     }
@@ -65,10 +120,17 @@ public class Picture
      */
     public void setBlackAndWhite()
     {
-        wall.changeColor("black");
-        window.changeColor("white");
-        roof.changeColor("black");
+        background.changeColor("white");
         sun.changeColor("black");
+        ground.changeColor("white");
+        person1.changeColor("black");
+        person2.changeColor("black");
+        ball.changeColor("black");
+        head.changeColor("black");
+        chillbaHat.changeColor("black");
+        neck.changeColor("black");
+        rightLense.changeColor("white");
+        leftLense.changeColor("white");
     }
 
     /**
@@ -76,9 +138,16 @@ public class Picture
      */
     public void setColor()
     {
-        wall.changeColor("red");
-        window.changeColor("black");
-        roof.changeColor("green");
+        background.changeColor("skyblue");
         sun.changeColor("yellow");
+        ground.changeColor("green");
+        person1.changeColor("black");
+        person2.changeColor("black");
+        ball.changeColor("black");
+        head.changeColor("lighttone");
+        chillbaHat.changeColor("red");
+        neck.changeColor("lighttone");
+        rightLense.changeColor("black");
+        leftLense.changeColor("black");
     }
 }
